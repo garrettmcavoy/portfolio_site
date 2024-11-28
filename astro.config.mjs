cab6501ddee905d76ec67a437087a82a73bab8c2
garrettmcavoy <garrettmcavoy@gmail.com>
2024-11-28 11:51:40 -0800

    lesgo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   // Your existing Astro configuration
   site: 'https://garrettmcavoy.github.io',
-  base: '/portfolio_site', // Replace <repository-name> with your GitHub repo name
+  base: '/portfolio_site/', // Replace <repository-name> with your GitHub repo name
 });
