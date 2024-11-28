aeb747702fa464393e386c3fcad3c82e21080427
garrettmcavoy <garrettmcavoy@gmail.com>
2024-11-28 11:27:12 -0800

    sure?

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // Your existing Astro configuration
-  base: '/portfolio_site/', // Replace <repository-name> with your GitHub repo name
-  outDir: 'dist', // Default output directory for Astro builds
+  site: 'https://garrettmcavoy.github.io'
+  base: '/portfolio_site', // Replace <repository-name> with your GitHub repo name
 });
