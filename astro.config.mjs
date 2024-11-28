728698377c7e0456078356b0208aa7032a52c6e0
garrettmcavoy <garrettmcavoy@gmail.com>
2024-11-28 11:38:07 -0800

    hai

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // Your existing Astro configuration
-  site: 'https://garrettmcavoy.github.io'
+  site: 'https://garrettmcavoy.github.io',
   base: '/portfolio_site', // Replace <repository-name> with your GitHub repo name
 });
